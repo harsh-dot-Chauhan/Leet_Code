fdcd8e5ca2ff02303bc2c748c043ebc483c6b8b3
unknown <iamthakurharshchauhan@gmail.com>
2022-07-28 18:56:48 +0530

    valid-anagram

---
+++ valid-anagram.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/valid-anagram
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sr = s.toCharArray();
+        char[] tr= t.toCharArray();
+        Arrays.sort(sr);
+        Arrays.sort(tr);
+        return String.valueOf(sr).equals(String.valueOf(tr));
+        
+    }
+}
